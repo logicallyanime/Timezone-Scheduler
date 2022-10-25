143b355c9e3c16448141506c897de52b5089cc55
Jordan341 <78373040+Jordan341@users.noreply.github.com>
2022-10-25 14:40:53 -0400

    Add files via upload

---
+++ userDAO.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+
+        <?php
+            class userDAO{
+                public $usr;
+                public function __construct(){
+                    usr = null;
+                }
+                public function set(u){
+                    usr = u;
+                }
+                public function get(){
+                    return usr;
+                }
+            }
+        ?>  
+
+    </body>
+</html>

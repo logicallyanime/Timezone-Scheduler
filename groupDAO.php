ac996d205f64ce7b93e9be32636b1a89120a1409
Jordan341 <78373040+Jordan341@users.noreply.github.com>
2022-10-25 14:42:07 -0400

    Add files via upload

---
+++ groupDAO.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+<?php
+class groupDAO{
+    public $grp;
+    public function __construct(){
+        grp = null;
+    }
+    public function set(g){
+        grp = g;
+    }
+    public function get(){
+        return grp;
+    }
+}
+?>
+
+</body>
+</html>
